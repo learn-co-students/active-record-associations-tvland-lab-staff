6462ec843bab7afd05f1d6fcf8b442dedaf369a1
github username <github email address>
2020-04-03 16:27:07 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
+	has_many :characters
+	has_many :actors, through: :characters
+	belongs_to :network
+
+	def actors_list
+		a = []
+		self.actors.each do |act|
+			a << act.first_name + " " + act.last_name 
+		end
+		a
+
+
+	end
   
 end
